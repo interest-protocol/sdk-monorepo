0b2abb2845be65dfb1f8e43f89d771ed4736ec20
Jose Cerqueira <josecerqueira@protonmail.com>
2025-05-17 06:51:16 +0100

    :chore: update interface

--- packages/v3-core/src/lib/types.ts
+++ packages/v3-core/src/lib/types.ts
@@ -1,6 +1,5 @@
 export interface FromTickArgs {
   tick: number;
-  baseToken: string;
-  quoteToken: string;
-  caller: string; // Address of the caller that must be authorized
+  tokenA: string;
+  tokenB: string;
 }
