7658865b764f54d6ac36550c3980eceb71a398af
Jose Cerqueira <josecerqueira@protonmail.com>
2025-06-17 21:10:25 +0100

    :recycle: chore: refactor on blizzard apr

--- scripts/blizzard/lst/apr.ts
+++ scripts/blizzard/lst/apr.ts
@@ -7,17 +7,18 @@ import { blizzardSDK } from '../utils.script';
 
 (async () => {
   const epoch = await blizzardSDK.getEpochData();
+  const ONE_WWAL = 1_000_000_000n;
 
   const [currentEpochRate, lastEpochRate] = await Promise.all([
     blizzardSDK.toWalAtEpoch({
       blizzardStaking: SHARED_OBJECTS.WWAL_STAKING({ mutable: true }),
       epoch: epoch.currentEpoch,
-      value: 1_000_000_000n,
+      value: ONE_WWAL,
     }),
     blizzardSDK.toWalAtEpoch({
       blizzardStaking: SHARED_OBJECTS.WWAL_STAKING({ mutable: true }),
       epoch: epoch.currentEpoch - 1,
-      value: 1_000_000_000n,
+      value: ONE_WWAL,
     }),
   ]);
 
