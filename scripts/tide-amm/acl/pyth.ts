b876f7f4356356881ecfe086389c3f6f176de5af
Jose Cerqueira <josecerqueira@protonmail.com>
2025-07-08 22:25:12 +0100

    :sparkles: feat: added script to update prices

--- scripts/tide-amm/acl/pyth.ts
+++ scripts/tide-amm/acl/pyth.ts
@@ -1,3 +1,4 @@
+import { logSuccess } from '@interest-protocol/logger';
 import { executeTx } from '@interest-protocol/sui-utils';
 import { sleep } from '@interest-protocol/sui-utils';
 import { TideSdk } from '@interest-protocol/tide-amm';
@@ -17,7 +18,7 @@ const PRICE_FEED_SUI_USD =
 const PRICE_FEED_USDC_USD =
   '0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a';
 
-(async () => {
+const updatePrice = async () => {
   const connection = new HermesClient('https://hermes.pyth.network', {});
 
   // https://www.pyth.network/developers/price-feed-ids
@@ -43,4 +44,22 @@ const PRICE_FEED_USDC_USD =
   });
 
   await executeTx(tx);
+};
+
+const tenMinutesLater = Date.now() + 10 * 60 * 1000;
+
+(async () => {
+  try {
+    while (tenMinutesLater > Date.now()) {
+      console.log('BEFORE updatePrice');
+
+      await updatePrice();
+
+      console.log('AFTER updatePrice - Updated price');
+
+      await sleep(1_500);
+    }
+  } catch (error) {
+    console.error('Error in loop:', error);
+  }
 })();
