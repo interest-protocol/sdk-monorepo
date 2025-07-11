9efe44d49b67be538ef1fef09b6e61ad2c9e5d21
Jose Cerqueira <josecerqueira@protonmail.com>
2025-07-11 19:24:28 +0100

    :sparkles: fix: add action

--- scripts/tide-amm/manager/set-fee-y.ts
+++ scripts/tide-amm/manager/set-fee-y.ts
@@ -1,7 +1,7 @@
 import { executeTx } from '@interest-protocol/sui-utils';
 import { TideSdk } from '@interest-protocol/tide-amm';
 
-import { ADMIN_TO_UPDATE, MOCK_SUI_MOCK_USDC_POOL } from '../utils.script';
+import { ADMIN_TO_UPDATE, SUI_USDC_POOL } from '../utils.script';
 
 const sdk = new TideSdk();
 
@@ -9,7 +9,7 @@ const admin = ADMIN_TO_UPDATE;
 
 (async () => {
   const tx = sdk.setFeeY({
-    pool: MOCK_SUI_MOCK_USDC_POOL,
+    pool: SUI_USDC_POOL,
     feeY: 0.001 * 1e6,
     admin,
   });
