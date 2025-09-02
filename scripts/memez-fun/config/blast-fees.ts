a008b004b37a64debd61ef2db6bf2d7286a9aae8
Jose Cerqueira <josecerqueira@protonmail.com>
2025-09-02 02:18:07 +0100

    :wrench: fix: wallet orders

--- scripts/memez-fun/config/blast-fees.ts
+++ scripts/memez-fun/config/blast-fees.ts
@@ -5,10 +5,10 @@ import { getEnv } from '../utils.script';
 const NEXA =
   '0x067a5b92098b8b2d21cbdf602031471e5940b4482a6e275d2dc6ef5370499fb7';
 
-const GIVEREP =
+const BLAST =
   '0x22441936a0d6fd21d07d596813dfa29fbc54d44b94eb87916fbcb51d639fde96';
 
-const BLAST =
+const GIVEREP =
   '0x28c6231133825003c823447a8c777a926c63ffc006fe18136052913f0de2b908';
 
 const IPX =
