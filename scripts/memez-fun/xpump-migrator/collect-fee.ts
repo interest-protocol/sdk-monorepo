473530eae38af450282d5dce632ba44eff86db6d
Jose Cerqueira <josecerqueira@protonmail.com>
2025-09-18 01:15:26 +0100

    :sparkles: feat: add vortex sdk

--- scripts/memez-fun/xpump-migrator/collect-fee.ts
+++ scripts/memez-fun/xpump-migrator/collect-fee.ts
@@ -3,22 +3,33 @@ import invariant from 'tiny-invariant';
 import { getEnv } from '../utils.script';
 
 (async () => {
-  const { network, keypair, executeTx, xPumpMigratorSdk } = await getEnv();
+  const {
+    network,
+    keypair,
+    executeTx,
+    xPumpMigratorSdk,
+    devInspectTransactionBlock,
+  } = await getEnv();
 
   invariant(network === 'mainnet', 'Only mainnet is supported');
 
   const { tx, suiCoin } = xPumpMigratorSdk.collectFee({
     bluefinPool:
-      '0x35906d9576411093fba737f99a8b563b31cc5cd7a29879cc7cd58ff7384c7631',
+      '0xe0a0cb9cf19d16a01590420e0bf6351760247ba87916ab1846b5aa36be3122f2',
     memeCoinType:
-      '0x41fe50c9964878ca4274fa798b1118084c2b1af4c3933d3d2ecce63e0611b017::last_test::LAST_TEST',
+      '0xefde5ddb743bd93e68a75e410e985980457b5e8837c7f4afa36ecc12bb91022b::mnm::MNM',
     positionOwner:
-      '0x1f6b7ed76647cb9e0d11c127bb5fd586749cd5768a61c55233c8469c7dc13595',
+      '0x23bdcfe9367ee38878d376831a6a9d6718801fbee7e12359bdd6c67c2ea822fc',
   });
 
   tx.transferObjects([suiCoin], tx.pure.address(keypair.toSuiAddress()));
 
   tx.setSender(keypair.toSuiAddress());
 
-  executeTx(tx);
+  console.log(
+    await devInspectTransactionBlock(
+      tx,
+      '0x7cd2ee66b1bf2561ba0a8f13515e05d700d4ef90d54adfb6948fec7b0c38a095'
+    )
+  );
 })();
