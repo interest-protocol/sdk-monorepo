99040a6806f87030b75804f9a4b98f37c5746765
Jose Cerqueira <josecerqueira@protonmail.com>
2025-12-16 15:21:05 +0000

    :sparkles: chore: improve merkle tree re-construction

--- packages/vortex/src/entities/merkle-tree.ts
+++ packages/vortex/src/entities/merkle-tree.ts
@@ -4,8 +4,8 @@ import { ZERO_VALUE, MERKLE_TREE_HEIGHT } from '../constants';
 import { poseidon2 } from '../crypto';
 import { Element, MerkleTree as FixedMerkleTree } from 'fixed-merkle-tree';
 
-export const buildMerkleTree = () =>
-  new FixedMerkleTree(MERKLE_TREE_HEIGHT, [], {
+export const buildMerkleTree = (elements: Element[] = []) =>
+  new FixedMerkleTree(MERKLE_TREE_HEIGHT, elements, {
     hashFunction: (left: Element, right: Element) =>
       poseidon2(BigInt(left), BigInt(right)).toString(),
     zeroElement: ZERO_VALUE.toString(),
