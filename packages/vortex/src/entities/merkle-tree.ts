faa3fdc1dc6791f3e1f5f7f58543383150d837b1
Jose Cerqueira <josecerqueira@protonmail.com>
2025-12-16 15:42:04 +0000

    :sparkles: feat: add merkle tree logic

--- packages/vortex/src/entities/merkle-tree.ts
+++ packages/vortex/src/entities/merkle-tree.ts
@@ -2,13 +2,22 @@
 
 import { ZERO_VALUE, MERKLE_TREE_HEIGHT } from '../constants';
 import { poseidon2 } from '../crypto';
-import { Element, MerkleTree as FixedMerkleTree } from 'fixed-merkle-tree';
+import {
+  Element,
+  MerkleTree as FixedMerkleTree,
+  SerializedTreeState,
+} from 'fixed-merkle-tree';
+
+export const merkleTreeHashFunction = (left: Element, right: Element) =>
+  poseidon2(BigInt(left), BigInt(right)).toString();
 
 export const buildMerkleTree = (elements: Element[] = []) =>
   new FixedMerkleTree(MERKLE_TREE_HEIGHT, elements, {
-    hashFunction: (left: Element, right: Element) =>
-      poseidon2(BigInt(left), BigInt(right)).toString(),
+    hashFunction: merkleTreeHashFunction,
     zeroElement: ZERO_VALUE.toString(),
   });
+
+export const deserializeMerkleTree = (data: SerializedTreeState) =>
+  FixedMerkleTree.deserialize(data, merkleTreeHashFunction);
 
 export type MerkleTree = FixedMerkleTree;
